0da8b9ce5ffd05c148be9ea51e1fef5334977259
Tiago Pitombeira <tiagopitombeira1984@gmail.com>
2025-06-03 16:10:14 -0300

    Avançando em React

--- 2_FUNDAMENTOS/fundamentos/src/App.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -1,6 +1,13 @@
-import './App.css'
-import FirstComponent from './components/FirstComponent'
+import './App.css';
+// Importando o componente Events
+import Events from './components/Events';
 // Importando o componente FirstComponent
+import FirstComponent from './components/FirstComponent'
+// Importando o componente MyComponent
+import MyComponent from './components/MyComponent';
+// Importando o componente TemplateExpression
+import TemplateExpression from './components/TemplateExpression';
+
 function App() {
 
   return (
@@ -8,6 +15,9 @@ function App() {
       <div className="App"> 
         <h1>Fundamentos React</h1>
         <FirstComponent /> {/* Componente criado */}
+        <TemplateExpression /> {/* Componente criado */}
+        <MyComponent/>
+        <Events/>
         <p>Começando com React</p>
       </div>
     </>
--- 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.jsx
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.jsx
@@ -1,8 +1,10 @@
+import MyComponent from "./MyComponent"
 
 const FirstComponent = () => {
     return (
         <div>
             <h1>Meu Primeiro Componente</h1> {/* Criando um componente */}
+            <MyComponent/> {/* Importando outro componente */}
         </div>
     )
 }
